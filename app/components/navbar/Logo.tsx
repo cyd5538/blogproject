eec6c9b3f35ef0c888f6f0d7ceb7c2b1c456a4f1
cyj <cyd9911@gmail.com>
2023-04-12 23:54:48 +0900

    헤더 디자인

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,19 @@
+"use client" 
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      className="cursor-pointer " 
+      src="/images/logo.png" 
+      height="50" 
+      width="50" 
+      alt="Logo" 
+    />
+  )
+}
+
+export default Logo
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { BiSearch } from 'react-icons/bi'
+
+
+const Search = () => {
+  return (
+    <div className="relative">
+      <div className="p-2 bg-rose-500 rounded-full text-white">
+        <BiSearch size={24} />
+      </div>
+    </div>
+  )
+}
+
+export default Search
