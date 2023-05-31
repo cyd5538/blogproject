aabc7ff2ea003b4e44a11877020b8e6b7e52dd88
cyd5538 <cyd5538@naver.com>
2023-05-31 14:36:49 +0900

    배포 에러 2번째 수정

--- app/components/blog/BlogPosts.tsx
+++ app/components/blog/BlogPosts.tsx
@@ -11,7 +11,7 @@ import Comment from "./Comment";
 
 import remarkGfm from 'remark-gfm'
 import ReactMarkdown from 'react-markdown'
-import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import { User } from "../types";
@@ -25,7 +25,7 @@ import BlogShare from "./BlogShare";
 import { TOC } from './Blogtoc';
 
 interface BlogPostsProps {
-  currentUser : User | null;
+  currentUser: User | null;
 }
 
 const Posts = async (id: string | string[] | undefined) => {
@@ -33,7 +33,7 @@ const Posts = async (id: string | string[] | undefined) => {
   return response.data
 }
 
-const BlogPosts:React.FC<BlogPostsProps> = ({currentUser}) => {
+const BlogPosts: React.FC<BlogPostsProps> = ({ currentUser }) => {
   const params = useParams();
 
   const { data, error, isLoading } = useQuery<SinglePostType[]>({
@@ -51,7 +51,7 @@ const BlogPosts:React.FC<BlogPostsProps> = ({currentUser}) => {
     }
     return text + child;
   };
-  
+
   type HeadingRendererProps = {
     level: number;
     children: ReactNode;
@@ -74,65 +74,65 @@ const BlogPosts:React.FC<BlogPostsProps> = ({currentUser}) => {
       <ul className="mb-4">
         {data?.[0].tags?.map((tag) => (
           <Link key={tag} href={`/tags?tag=${tag}`}>
-            <li  className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <li className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               {tag}
             </li>
           </Link>
         ))}
       </ul>
       <div className="bg-white p-4">
-      <div className='flex gap-4'>
-        <div className='content w-full lg:w-3/4'>
-          {data?.[0].content && (
+        <div className='flex gap-4'>
+          <div className='content w-full lg:w-3/4'>
+
             <ReactMarkdown
-                    children={data?.[0].content} 
-                    remarkPlugins={[remarkGfm]}
-                    className="post-content"
-                    components={{
-                        code({node, inline, className, children, ...props}) {
-                            const match = /language-(\w+)/.exec(className || '')
-                            return !inline && match ? (
-                            <SyntaxHighlighter
-                              language={match[1]}
-                              PreTag="div"
-                              {...props}
-                            >
-                              {String(children).replace(/\n$/, '')}
-                            </SyntaxHighlighter>
-                            ) : (
-                            <code className={className} {...props}>
-                                {children}
-                            </code>
-                            )
-                        },
-                        h1: HeadingRenderer,
-                        h2: HeadingRenderer,
-                    }}
-                />
-              )}
+              children={data?.[0].content || ""}
+              remarkPlugins={[remarkGfm]}
+              className="post-content"
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '')
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      language={match[1]}
+                      PreTag="div"
+                      {...props}
+                    >
+                      {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  )
+                },
+                h1: HeadingRenderer,
+                h2: HeadingRenderer,
+              }}
+            />
+
           </div>
-        <div className='w-40 hidden relative top-0 right-0 lg:block'>
-          <div className='fixed'>
-            <TOC selector=".content" />
+          <div className='w-40 hidden relative top-0 right-0 lg:block'>
+            <div className='fixed'>
+              <TOC selector=".content" />
+            </div>
           </div>
-        </div>
         </div>
       </div>
       <div className="flex items-center justify-between border-b-2 pb-2 pt-4">
         <div className="flex justify-between gap-2 w-full">
           <div className="flex items-center gap-2">
-            <Avatar src={data?.[0].user?.image} width={50} height={50}/>
+            <Avatar src={data?.[0].user?.image} width={50} height={50} />
             <div>
               <div className="text-lg text-gray-500 dark:text-white">{data?.[0].user?.email}</div>
               <div className="text-lg font-medium text-gray-900 dark:text-white">{data?.[0].user?.name}</div>
             </div>
           </div>
-          <BlogShare title={data?.[0].title} tags={data?.[0].tags}/>
+          <BlogShare title={data?.[0].title} tags={data?.[0].tags} />
         </div>
       </div>
-      <Comment 
+      <Comment
         id={data?.[0].id}
-        comment={data?.[0].comments} 
+        comment={data?.[0].comments}
         currentUser={currentUser}
       />
 
